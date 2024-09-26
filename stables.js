7c2f3b0dc921f2fc21dd9bbb22b7145c634c7c37
장예진 <jang-yejin@jang-yejin-ui-MacBookAir.local>
2024-09-26 16:13:40 -0700

    this is my class rab activity

--- stables.js
+++ stables.js
@@ -9,6 +9,8 @@ console.log(isHorseInStable);
 horseName = "Pretty"
 console.log(horseName);
 
+
+
 console.log(typeof 890);
 console.log(typeof "890");
 console.log(typeof horseAge);
@@ -22,12 +24,27 @@ console.log(STABLE_MONTHLY_FEE * 123);
 console.log(STABLE_MONTHLY_FEE / 50);
 
 
-let horseIntroduction = " is the name of my horse";
-// let rentIntroduction = "it costs" + STABLE_MONTHLY_FEE + " to board " + horseName;
-let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
-// let stableIntroduction = "Welcome to " + horseName + "'s stable ";
-let stableIntroduction = `Welcome to "${horseName}'s" stable`;
-console.log(rentIntroduction);
+
+////////LESSON $ CODE //////
+let horseNickname = "swift horse in the world!";
+console.log(horseNickname);
+
+let horseIntroduction = `${horseName} is my horse's name, and nickname of pretty is ${horseNickname}`;
+console.log(horseIntroduction);
+
+
+let feeIntroduction = `The monthly fee is usually, ${STABLE_MONTHLY_FEE} per month.`;
+console.log(feeIntroduction);
+
+let discoutEvent = `Get 10% discount for three month!, you purchase only $ ${STABLE_MONTHLY_FEE*3*0.9}`;
+console.log(discoutEvent);
+
+
+
+
+
+
+
 
 
 

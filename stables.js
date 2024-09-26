881ed1b88c6fab4de8e6a16351e68756bb897ef2
장예진 <jang-yejin@jang-yejin-ui-MacBookAir.local>
2024-09-26 14:43:06 -0700

    add operators to my stable file

--- stables.js
+++ stables.js
@@ -24,9 +24,9 @@ console.log(STABLE_MONTHLY_FEE / 50);
 
 let horseIntroduction = " is the name of my horse";
 // let rentIntroduction = "it costs" + STABLE_MONTHLY_FEE + " to board " + horseName;
-let rentIntroduction = 'It costs ${STABLE_MONTHLY_FEE} to board $(horseName)`;
-//let stableIntroduction = "Welcome to " + horseName + "'s stable ";
-let stableIntroduction = 'Welcome to "${horseName}'s" stable`;
+let rentIntroduction = 'It costs ${STABLE_MONTHLY_FEE} to board $(horseName)'`;
+// let stableIntroduction = "Welcome to " + horseName + "'s stable ";
+let rentIntroduction = 'Welcome to "${horseName}'s" stable`;
 console.log(rentIntroduction);
 
 

81fad4cc25421ddc676e13fa6e81af2023cd84ae
장예진 <jang-yejin@jang-yejin-ui-MacBookAir.local>
2024-09-26 14:36:38 -0700

    add operators to my stable file

--- stables.js
+++ stables.js
@@ -4,7 +4,30 @@ horseName = "Remarkable"; // for strings, prefer double quotes
 horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
-let isHorseInStable = false;  
+let isHorseInStable = true;  
 console.log(isHorseInStable);
 horseName = "Pretty"
 console.log(horseName);
+
+console.log(typeof 890);
+console.log(typeof "890");
+console.log(typeof horseAge);
+
+const STABLE_MONTHLY_FEE = 100;
+
+
+
+console.log(STABLE_MONTHLY_FEE + STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE * 123);
+console.log(STABLE_MONTHLY_FEE / 50);
+
+
+let horseIntroduction = " is the name of my horse";
+// let rentIntroduction = "it costs" + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = 'It costs ${STABLE_MONTHLY_FEE} to board $(horseName)`;
+//let stableIntroduction = "Welcome to " + horseName + "'s stable ";
+let stableIntroduction = 'Welcome to "${horseName}'s" stable`;
+console.log(rentIntroduction);
+
+
+
